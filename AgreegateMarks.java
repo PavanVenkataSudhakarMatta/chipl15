aef59a69fc6475400888e8c8c6ae14545c83ed6c
Praveen <praveenkumarpr1814@gmail.com>
2023-12-19 10:49:27 +0530

    praveen kumar

---
+++ AgreegateMarks.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+public class AgreegateMarks {
+
+	public static void main(String[] args) {
+		int m1=89,m2=56,m3=78,m4=87,m5=85,m6=96;
+		int total=m1+m2+m3+m4+m5+m6; //491
+		int max_marks=600;       //600
+		double result=(total*100/max_marks);  //491/600*100
+		System.out.println(result);
+		
+	}
+
+}
